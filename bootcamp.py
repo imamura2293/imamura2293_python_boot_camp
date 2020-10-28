9c9f3041959bef6e716fd8348ff3b300c34069ca
imamura2293 <uu77ee22dd55>
2020-10-28 22:15:10 +0900

    boot

---
+++ bootcamp.py
@@ -0,0 +1,47 @@
+2 + 2
+
+3 - 8
+6 * 9
+8 / 2
+5 % 2
+
+width = 60
+height = 90
+width * height
+
+"Hello world"
+
+"Monty Python's Flying Circus"
+
+["Hello", 3]
+
+# ここはコメント文
+a = 1
+
+
+def add(a, b):
+    return a + b
+
+
+add(1, 3)
+
+round(10.4)
+
+
+def fizzbuzz(num):
+    retrun
+    num
+
+
+print(fizzbuzz(4))
+
+
+def fizzbuzz(num):
+    return num
+
+
+for num in range(1, 101):
+    print(fizzbuzz(num))
+
+
+def fizzbuzz(num):
